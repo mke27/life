8445c8dae911721ae90a2ba2d64c408c5137c46b
Max Robinson <robinson.max@northeastern.edu>
2025-05-27 19:45:26 +0200

    inverse sigmoid fix

--- ml-src/cos_similarity.py
+++ ml-src/cos_similarity.py
@@ -6,24 +6,50 @@
 import numpy as np
 import pandas as pd
 
-# education, infrastructure, environment, safety
-input = np.array([1, 0.5, 0.2, 0.1])
+
+# reads in the CSV as df
+df_all_years = pd.read_csv('datasets/preprocessed-datasets/all_data.csv')
+
+# filters all data points that don't include a year of 2022
+recent = (df_all_years.year == 2022)
+df = df_all_years[recent]
+
+# healthcare, education, safety, environment, infrastructure -> mock input
+input = np.array([0, 0, 0, 0, 0]) # NOTE!!! REVERSE SIGMOID THE INPUTS FOR REAL INPUTS
+
+def inv_sigmoid(value):
+    """ returns the inverse sigmoid of the input. If the input is 0 or 1, rebounds the sigmoid to -3 or 3
+        Args:
+            value (int): value to be inversed sigmoided
+        Returns:
+            inv_sig (int): inverse sigmoid of input bounded between -3 and 3
+    """
+    if value == 1:
+        value = .95
+    elif value == 0:
+        value = 0.05
+
+    return np.log(value) - np.log(1-value)
+
+inv_sig_input = np.array(list(map(inv_sigmoid, input)))
 
 # makes a list of each country with that countries vector inside
-country_vector = [np.array(df.iloc[row, 0], 
-                           df.iloc[row, 1], 
-                           df.iloc[row, 2], 
-                           df.iloc[row, 3]) for row in range(len(df))]
+# country_vector = [np.array(df.iloc[row, 0], 
+#                            df.iloc[row, 1], 
+#                            df.iloc[row, 2], 
+#                            df.iloc[row, 3]) for row in range(len(df))]
+
 
 cos_scores = []
 
 for country in range(len(df)):
-    temp_vector = np.array(df.iloc[country, 0], 
-                           df.iloc[country, 1], 
-                           df.iloc[country, 2], 
-                           df.iloc[country, 3])
+    temp_vector = np.array([df.iloc[country, 2], 
+                           df.iloc[country, 3], 
+                           df.iloc[country, 4], 
+                           df.iloc[country, 5],
+                           df.iloc[country, 6]])
     
-    cos_similarity = np.dot(input, temp_vector) / (np.linalg.norm(input) * np.linalg.norm(temp_vector))
+    cos_similarity = np.dot(inv_sig_input, temp_vector) / (np.linalg.norm(inv_sig_input) * np.linalg.norm(temp_vector))
 
     cos_scores.append(cos_similarity)
 
@@ -34,4 +60,4 @@ df_scores = pd.DataFrame(dict_scores)
 
 sorted_df_scores = df_scores.sort_values('similarity', ascending = False)
 
-
+print(sorted_df_scores)
