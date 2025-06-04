3285b3bf96bf13cfe7afdee926a04a4832635bb0
Max Robinson <robinson.max@northeastern.edu>
2025-06-04 17:48:08 +0200

    cos similarity changes

--- api/backend/ml_models/model01.py
+++ api/backend/ml_models/model01.py
@@ -5,7 +5,54 @@ accessed.
 """
 from backend.db_connection import db
 import numpy as np
+import pandas as pd
 # import logging
+
+def inv_sigmoid(value):
+    """ returns the inverse sigmoid of the input. If the input is 0 or 1, rebounds the sigmoid to -3 or 3
+        Args:
+            value (int): value to be inversed sigmoided
+        Returns:
+            inv_sig (int): inverse sigmoid of input bounded between -3 and 3
+    """
+    if value == 1:
+        value = .95
+    elif value == 0:
+        value = 0.05
+
+    return np.log(value) - np.log(1-value)
+
+def cosine_similarity(df, input_vector):
+    """ returns a sorted dataframe of countries and their cosine similarity scores compared to the input vector.
+        Args:
+            df (pandas DataFrame): dataframe of all country's most recent data for all five features.
+            input_vector (numpy.array): array of the input of the sliders in the order healthcare, education, safety, environment, infrastructure.
+        Returns:
+            sorted_df_scores (pandas DataFrame): a sorted dataframe of countries and their cosine similarity scores compared to the input vector.
+    """
+    cos_scores = []
+
+    inv_sig_input = np.array(list(map(inv_sigmoid, input_vector)))
+
+    for country in range(len(df)):
+        temp_vector = np.array([df.iloc[country, 2], 
+                            df.iloc[country, 3], 
+                            df.iloc[country, 4], 
+                            df.iloc[country, 5],
+                            df.iloc[country, 6]])
+        
+        cos_similarity = np.dot(inv_sig_input, temp_vector) / (np.linalg.norm(inv_sig_input) * np.linalg.norm(temp_vector))
+
+        cos_scores.append(cos_similarity)
+
+    dict_scores = {'Country_input': df.country,
+                'similarity': cos_scores}
+
+    df_scores = pd.DataFrame(dict_scores)
+
+    sorted_df_scores = df_scores.sort_values('similarity', ascending = False)
+    return sorted_df_scores
+
 
 from flask import current_app
 
@@ -26,10 +73,12 @@ def predict(var01, var02):
   # get a database cursor 
   cursor = db.get_db().cursor()
   # get the model params from the database
-  query = 'SELECT beta_vals FROM model1_params ORDER BY sequence_number DESC LIMIT 1'
+  query = 'SELECT * FROM ML_Score'
   cursor.execute(query)
-  return_val = cursor.fetchone()
+  return_val = cursor.fetchall()
   params = return_val['beta_vals']
+
+  cosine_similarity()
   
   # turn the values from the database into a numpy array
   params_array = np.array(list(map(float, params[1:-1].split(','))))
@@ -43,3 +92,5 @@ def predict(var01, var02):
 
   return prediction
 
+# just remove infrastructure
+# 10 years, no 2013.
