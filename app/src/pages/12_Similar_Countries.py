65cdc5b0fdf219614b1ff45c2a57c7b4090bce15
Max Robinson <robinson.max@northeastern.edu>
2025-06-09 17:22:08 +0200

    policy news feature

--- app/src/pages/12_Similar_Countries.py
+++ app/src/pages/12_Similar_Countries.py
@@ -2,6 +2,7 @@ import logging
 logger = logging.getLogger(__name__)
 import streamlit as st
 import requests
+import pandas as pd
 from streamlit_extras.app_logo import add_logo
 from modules.nav import SideBarLinks
 
@@ -12,6 +13,9 @@ style_sidebar()
 st.header('Similar Countries')
 COUNTRY_API_URL = "http://web-api:4000/country/countries"
 country_response = requests.get(COUNTRY_API_URL)
+
+MODEL_API_URL = "http://web-api:4000/faye/scores"
+scores_response = requests.get(MODEL_API_URL)
 
 if(country_response.status_code == 200):
     countries = country_response.json()
@@ -35,4 +39,13 @@ if st.button(f"Country most similar to {option}:"):
     
 if st.session_state.show_sim_country:
     #get the most similar country based on country --- route
+    model_response_json = scores_response.json()
+
+    # df = pd.DataFrame.from_dict(model_response_json)
+    # df['environment_score'] = -df['environment_score']
+    # df['safety_score'] = -df['safety_score']
+
+    # compare_to = df[]
+
+    # df_sorted = df.sort_values(input_issue, ascending=True)
     st.write('Belgium')
