42add7c3263e6fa8037fff024f9d139fc3a7b417
Max Robinson <robinson.max@northeastern.edu>
2025-06-12 14:20:24 +0200

    fixed bug with relative scores calculations not working

--- app/src/pages/12_Similar_Countries.py
+++ app/src/pages/12_Similar_Countries.py
@@ -159,7 +159,7 @@ if st.session_state.show_sim_country:
         st.plotly_chart(fig, use_container_width=True, theme="streamlit")
 
     with col2:
-        st.dataframe(df_renamed.iloc[1:][["Country", "Similarity Score"]], use_container_width=True, height=400)
+        st.dataframe(df_renamed.iloc[1:][["Country", "Similarity Score"]].reset_index(drop=True), use_container_width=True, height=400)
 
     st.divider()
     st.subheader("Quality of Life Comparison")
